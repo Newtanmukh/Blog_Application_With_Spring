b9ca83df261a6240db397e0b2f9021b1819ee29a
Newtanmukh <190010027@iitdh.ac.in>
2026-04-24 17:36:46 +0530

    Basic Http Authentication

---
+++ demo/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /***
+     * In older versions of spring boot,
+     * we had WebSecurityConfigurerAdapter
+     * ***/
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable).
+                authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                //form based auth ended, now toolbax based will come.
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+}
